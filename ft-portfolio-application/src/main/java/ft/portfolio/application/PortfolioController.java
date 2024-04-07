11ae5820f880598a11e9e98ba17e07a2624aa83b
Kamilo <kamil.kazmierczak@pm.me>
2024-04-07 17:49:30 +0200

    Removed unused imports.

--- ft-portfolio-application/src/main/java/ft/portfolio/application/PortfolioController.java
+++ ft-portfolio-application/src/main/java/ft/portfolio/application/PortfolioController.java
@@ -2,14 +2,8 @@ package ft.portfolio.application;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/portfolio")
